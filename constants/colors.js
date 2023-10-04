67d765f2bd622dffb238762e6338841ca1550b67
Emil Beski <beskiemil@gmail.com>
2023-10-05 00:42:38 +0200

    Added color constants

--- constants/colors.js
+++ constants/colors.js
@@ -1,3 +1,17 @@
-const Colors = {};
+const Colors = {
+  primary400: "#5650e1",
+  primary500: "#362FD9",
+  primary600: "#231ad5",
+  primary700: "#1b14bd",
+  secondary400: "#20cccc",
+  secondary500: "#1AACAC",
+  secondary600: "#148383",
+  warning400: "#f64246",
+  warning500: "#FE2328",
+  warning600: "#fc161b",
+  accent: "#2E97A7",
+  background: "#EEEEEE",
+  white: "#FFFFFF",
+};
 
 export default Colors;
